64135b5800642bbe19eadddf4b2ce41e7114c4a1
Rizal Nugroho <rizalnug2@gmail.com>
2025-02-22 17:43:10 +0700

    add screenshoot function

--- test/loginTest.js
+++ test/loginTest.js
@@ -2,6 +2,8 @@ const { Builder } = require("selenium-webdriver");
 const LoginPage = require("../pages/loginPage");
 const InventoryPage = require("../pages/inventoryPage");
 const data = require("../fixtures/dataFile.json");
+const fs = require("fs");
+const path = require("path");
 
 async function sauceDemoLoginTest() {
   describe("Saucedemo Login Test", function () {
@@ -49,7 +51,22 @@ async function sauceDemoLoginTest() {
       console.log(data.log.loginFailed);
     });
 
-    afterEach(async () => {
+    afterEach(async function () {
+      const screenshotDir = path.join(__dirname, "../screenshots");
+      if (!fs.existsSync(screenshotDir)) {
+        fs.mkdirSync(screenshotDir);
+      }
+
+      // Gunakan nama test case untuk screenshot
+      const testCaseName = this.currentTest.title.replace(/\s+/g, "_"); // Ganti spasi dengan underscore
+
+      // Simpan screenshot baru dengan nama test case
+      const image = await driver.takeScreenshot();
+      fs.writeFileSync(
+        path.join(screenshotDir, `${testCaseName}_new.png`),
+        image,
+        "base64"
+      );
       await driver.quit();
     });
   });
